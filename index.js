22e38a3d97aee2675f9b0c168e23f6eabc162912
Molise Mahasele <molisemahasele@yahoo.com>
2023-10-17 16:55:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+//cors is middleware that enables cross origin requests
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+//root route
+app.get("/", (req, res) => {
+    res.send('Server is running');
+});
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("calluser", { signal: signalData, from, name }); 
+    });
+
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted", data.signal);
+    });
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
